83dd43975353012ff52b5a6423c7a2bedcf4f5ed
snowy <snowydg@proton.me>
2024-12-23 10:23:22 -0600

    initial file commit

---
+++ core/app.go
@@ -0,0 +1,10 @@
+package core
+
+// Defines the version and other Info
+type App struct {
+	version string
+}
+
+var app = App{
+	version: "0",
+}
---
+++ pikastore.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/pikastore/pikastore/core"
+)
+
+func main() {
+	core.Execute()
+}
