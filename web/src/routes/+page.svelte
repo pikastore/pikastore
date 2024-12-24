1fcd1e512a3570fefb2f5de165e718f6bc46514e
ros-e <snowydg@proton.me>
2024-12-23 19:32:58 -0600

    alot of work to do...

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,2 +1,8 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div class="login-page">
+    <h1>pikastore</h1>
+    <form class="auth-form">
+    <input placeholder="username" type="text" />
+    <input placeholder="password" type="password" />
+    <button>Register</button>
+    </form>
+</div>
