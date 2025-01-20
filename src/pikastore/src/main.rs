ff6f3157f50ef9d8d5f3e23fc60911028dc5565b
rose <snowydg@proton.me>
2025-01-20 01:09:21 -0600

    Update Cargo configuration and enhance command-line interface with new subcommands

--- src/pikastore/src/main.rs
+++ src/pikastore/src/main.rs
@@ -1,26 +1,59 @@
 /* ---ARGS---
-    serve (additional args: -port <i64> --host <String> )
-    
-    create bucket (additional args: -name <String> )
+    donate
+    serve (additional args: --port <i64> --host <String> ) 
+    secrets (additional args: generate ) (write these down you wont be able to see them again)
+    create bucket additional args: -name <String> )
 */
-use clap::Parser;
+use clap::{Parser, Subcommand};
 use rocket::{tokio, Config};
 use apis::server::server;
-
+use services::cmd::secrets;
 #[derive(Parser, Debug)]
 #[command(version, about, long_about = None)]
-struct Args {
-  #[arg(short, long)]
-   port: u16,
+pub struct Args {
+    #[command(subcommand)]
+    pub command: Commands,
+}
+#[derive(Subcommand, Debug)]
+pub enum Commands {
+    Serve {
+        #[arg(long, default_value = "8000")]
+        port: u16,
+        #[arg(long, default_value = "127.0.0.1")]
+        host: String,
+    },
+    Secrets {
+        #[arg(long)]
+        generate: bool,
+    },
+    Bucket {
+        #[arg(long)]
+        name: String,
+    },
 }
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
-    let config = Config {
-        port: args.port,
-        ..Config::default()
-    };
 
-    server(config).await.launch().await?;
- Ok(())
+    match &args.command {
+        Commands::Serve { host, port } => {
+            let config = Config {
+                address: host.parse()?,
+                port: *port, 
+                ..Config::default()
+            };
+            server(config).await.launch().await?;
+        }
+        Commands::Secrets { generate } => {
+            if *generate {
+                secrets::generate_key_id().await;
+                secrets::generate_access_key().await;
+            }
+        }
+        Commands::Bucket { name } => {
+            println!("Creating bucket with name: {}", name);
+        }
+    }
+
+    Ok(())
 }
---
+++ src/services/cmd/secrets.rs
@@ -0,0 +1,21 @@
+use rand::RngCore;
+use sha1::Digest;
+use sha256::digest;
+
+pub async fn generate_key_id() {
+    let mut rng = rand::thread_rng();
+    let mut random_bytes = [0u8; 10];
+    rng.fill_bytes(&mut random_bytes);
+    let hash = hex::encode(sha1::Sha1::digest(&random_bytes));
+
+    println!("Key ID: {}", hash);
+}
+
+pub async fn generate_access_key() {
+    let mut rng = rand::thread_rng();
+    let mut random_bytes = [0u8; 10];
+    rng.fill_bytes(&mut random_bytes);
+    let hash = digest(&random_bytes);
+
+    println!("Secret Access Key: {}", hash);
+}
